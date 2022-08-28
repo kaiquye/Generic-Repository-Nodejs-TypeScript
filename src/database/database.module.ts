fbbb4cb119fa5f28c92cc8432f3b6473621e429a
kaiquye <kaiquemendesilva82@gmail.com>
2022-08-28 17:30:48 -0300

    modules: fix modules

---
+++ src/database/database.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { UserController } from './user.controller';
+
+@Module({
+  controllers: [UserController],
+  providers: [],
+})
+export class UserModule {}
